7b100ebb10171e1fc9bd54f855f3987cc4ed006a
Utsab Gayawali <utsavgayawali@gmail.com>
2025-06-24 21:08:02 +0545

    3rd commit

--- postapp/settings.py
+++ postapp/settings.py
@@ -125,6 +125,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+ROOT_URLCONF = 'postapp.urls'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
